c4d7eb76ff611e17ad7c582f0e81c105df4298f7
TheRhettThompson <rthompsoncareer@gmail.com>
2022-10-26 01:45:15 +0000

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boile</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastname} </h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country} </h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -52,7 +52,7 @@ function render(variables = {}) {
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should
-    includeCover: true,
+    includeCover: "Include Cover",
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
@@ -64,11 +64,11 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "First",
+    lastname: "Last",
+    role: "Job Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); //render the card for the first time
 
